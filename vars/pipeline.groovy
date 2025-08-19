e829bc6f9564bd793d42864d1238b126c6068840
OLV3RAG <daaaayv@gmail.com>
2025-08-19 13:12:35 -0600

    UPDATE 5

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -6,12 +6,6 @@ pipeline {
     }
 
     stages {
-        stage('Checkout') {
-            steps {
-                checkout scm
-            }
-        }
-
         stage('Build') {
             steps {
                 echo '🔨 Compilando la aplicación...'
